f37cd8f33e13202976ef3d5298b62c34171c5200
dustin <dustywho@gmail.com>
2010-05-26 02:30:11 -0400

    make Occurence more ruby like

--- app/models/occurrence.rb
+++ app/models/occurrence.rb
@@ -3,11 +3,11 @@ require 'ice_cube'
 class Occurrence < ActiveRecord::Base
   belongs_to :task
   
-  def set_schedule(schedule)
+  def schedule=(schedule)
      self.ice_cube_yaml = schedule.to_yaml
   end
   
-  def get_schedule()
+  def schedule
     IceCube::Schedule.from_yaml(ice_cube_yaml)
   end
   
